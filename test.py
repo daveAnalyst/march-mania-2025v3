3e9c288495a7af4a9193d07fdde428b43173eb8d
dave <davidomungala3@gmail.com>
2025-04-03 11:25:48 -0700

    Feat: Complete baseline TF pipeline runs successfully (OOF Brier: [0.2500])

---
+++ test.py
@@ -0,0 +1,8 @@
+
+import sys
+import pandas as pd
+import tensorflow as tf
+
+print("Python version:", sys.version)
+print("Pandas version:", pd.__version__)
+print("Tensorflow version:", tf.__version__)
